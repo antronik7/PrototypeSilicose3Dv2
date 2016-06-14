35c7647445919b2e7156fe61dffd78cd6fbdedc1
Christian Lavoie <cristianlavoie111@gmail.com>
2016-06-14 11:37:24 -0400

    D/placement d<objet et v2 du hud selon les preference que benoit avait citer durant une presentation. Le hud fonctionne completement maintenant

--- PrototypeSilicose3D/Assets/Script/Chri/CarteCliqueControlleur.cs
+++ PrototypeSilicose3D/Assets/Script/Chri/CarteCliqueControlleur.cs
@@ -9,8 +9,7 @@ public class CarteCliqueControlleur : MonoBehaviour {
     [SerializeField] GameObject derniereCarte;
     [SerializeField] GameObject[] listeCartes;
     [SerializeField] int maxClique;
-    [SerializeField] Color bon;
-    [SerializeField] Color mauvais;
+    [SerializeField] GameObject mauvais;
 
     List<int> listeDeCarte;
     List<GameObject> bonneCarte;
@@ -35,27 +34,31 @@ public class CarteCliqueControlleur : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	
+
 	}
 
     public void joueurACliquer(int numeroCarte)
     {
-        if(numeroCarte != 0)
-        {           
-            //listeCartes[compteurBon].GetComponent<Image>().color = bon;
-            listeDeCarte.Add(numeroCarte);
-            listeDeCarte.Sort();
-            compteurBon++;
-            updateBonneCarte();
-        }
-        else
-        {
-            compteurMauvais++;
-            listeCartes[maxClique - compteurMauvais].GetComponent<Image>().color = mauvais; 
-        }
+        if(compteurClique < maxClique)
+        { 
+            if(numeroCarte != 0)
+            {           
+                //listeCartes[compteurBon].GetComponent<Image>().color = bon;
+                listeDeCarte.Add(numeroCarte);
+                listeDeCarte.Sort();
+                compteurBon++;
+                updateBonneCarte();
+            }
+            else
+            {
+                compteurMauvais++;
+                GameObject temp = (GameObject)Instantiate(mauvais, new Vector3(listeCartes[maxClique - compteurMauvais].transform.position.x, listeCartes[maxClique - compteurMauvais].transform.position.y, 1), Quaternion.Euler(rotation));
+                temp.transform.parent = listeCartes[maxClique - compteurMauvais].transform; 
+            }
 
-        compteurClique++;
-        updateDerniereCarte();    
+            compteurClique++;
+            updateDerniereCarte();
+        }   
     }
 
     void updateDerniereCarte()
@@ -65,8 +68,12 @@ public class CarteCliqueControlleur : MonoBehaviour {
         if(compteurBon == 12)
         {
             //Faire appel au gameover somehow
-            bonneCarte.Add((GameObject)Instantiate(carteASpawner[12], new Vector3(listeCartes[12].transform.position.x, listeCartes[12].transform.position.y, 1), Quaternion.Euler(rotation)));
+            bonneCarte.Add((GameObject)Instantiate(carteASpawner[0], new Vector3(listeCartes[12].transform.position.x, listeCartes[12].transform.position.y, 1), Quaternion.Euler(rotation)));
             bonneCarte[12].transform.parent = listeCartes[12].transform;
+            listeDeCarte.Add(0);
+            listeDeCarte.Sort();
+            compteurBon++;
+            updateBonneCarte();
         }
     }
     
@@ -76,7 +83,7 @@ public class CarteCliqueControlleur : MonoBehaviour {
 
         for(int i = 0; i < compteurBon; i++)
         {
-            bonneCarte.Add((GameObject)Instantiate(carteASpawner[listeDeCarte[i]-1], new Vector3(listeCartes[i].transform.position.x, listeCartes[i].transform.position.y, 1), Quaternion.Euler(rotation)));
+            bonneCarte.Add((GameObject)Instantiate(carteASpawner[listeDeCarte[i]], new Vector3(listeCartes[i].transform.position.x, listeCartes[i].transform.position.y, 1), Quaternion.Euler(rotation)));
             bonneCarte[i].transform.parent = listeCartes[i].transform;
         }
     }    
